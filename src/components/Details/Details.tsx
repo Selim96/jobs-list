56b9ae3d6cc1aa69b01f7494e1b86abee2cb460a
Selim Fazylov <selim.fazylov1@gmail.com>
2022-11-16 00:52:44 +0200

    start details

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "../../redux/hooks";
+import Loader from "../Loader";
+import s from "./Details.module.scss";
 
 const Details: React.FC = () => {
+    useEffect(() => {
+        
+    }, []);
+
+
     return (
-        <div>
-            Details page
+        <div className={s.detailsPage}>
+            <header>
+                <p>Job Details</p>
+                <div>
+                    <svg width={15} height={15}>
+
+                    </svg>
+                    <div className={s.share}>
+                        <svg width={15} height={15}></svg>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </header>
+            
         </div>
     )
 }
